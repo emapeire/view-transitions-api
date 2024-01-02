8a3954b877a0400259d31cc822103bc0849bcf6f
Emanuel Peire <emapeire@outlook.com>
2024-01-02 17:03:53 -0300

    Refactor BookItem and ListOfBooks components

---
+++ src/data/books.js
@@ -0,0 +1,32 @@
+export const books = [
+	{
+		id: 'clean-code',
+		title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg'
+	},
+	{
+		id: 'the-clean-coder',
+		title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg'
+	},
+	{
+		id: 'the-pragmatic-programmer',
+		title: 'The Pragmatic Programmer: From Journeyman to Master',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+	},
+	{
+		id: 'software-craftsman',
+		title: 'Software Craftsman, The: Professionalism, Pragmatism, Pride',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1416778735i/23215733.jpg'
+	},
+	{
+		id: 'eloquent-javascript',
+		title: 'Eloquent JavaScript: A Modern Introduction to Programming',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg'
+	},
+	{
+		id: 'javascript-the-good-parts',
+		title: 'JavaScript: The Good Parts',
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg'
+	}
+]
