3815612f7037d88e65d3f4ece99cf36c515257d9
Emanuel Peire <emapeire@outlook.com>
2024-01-02 17:43:45 -0300

    Add book opinions and authors to books.js

--- src/data/books.js
+++ src/data/books.js
@@ -2,31 +2,48 @@ export const books = [
 	{
 		id: 'clean-code',
 		title: 'Clean Code: A Handbook of Agile Software Craftsmanship',
-		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg'
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1436202607i/3735293.jpg',
+		opinion:
+			'This book is a must-read for every developer. It will help you to write better code and become a better developer.',
+		author: 'Robert C. Martin'
 	},
 	{
 		id: 'the-clean-coder',
 		title: 'The Clean Coder: A Code of Conduct for Professional Programmers',
-		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg'
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1347470803i/10284614.jpg',
+		opinion:
+			'This book is a must-read for every developer. It will help you to write better code and become a better developer.',
+		author: 'Robert C. Martin'
 	},
 	{
 		id: 'the-pragmatic-programmer',
 		title: 'The Pragmatic Programmer: From Journeyman to Master',
-		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg'
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1401432508i/4099.jpg',
+		opinion:
+			'This book is a must-read for every developer. It will help you to write better code and become a better developer.',
+		author: 'Andy Hunt'
 	},
 	{
 		id: 'software-craftsman',
 		title: 'Software Craftsman, The: Professionalism, Pragmatism, Pride',
-		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1416778735i/23215733.jpg'
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1416778735i/23215733.jpg',
+		opinion:
+			'This book is a must-read for every developer. It will help you to write better code and become a better developer.',
+		author: 'Sandro Mancuso'
 	},
 	{
 		id: 'eloquent-javascript',
 		title: 'Eloquent JavaScript: A Modern Introduction to Programming',
-		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg'
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1308260856i/8910666.jpg',
+		opinion:
+			'This book is a must-read for every developer. It will help you to write better code and become a better developer.',
+		author: 'Marijn Haverbeke'
 	},
 	{
 		id: 'javascript-the-good-parts',
 		title: 'JavaScript: The Good Parts',
-		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg'
+		img: 'https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1328834793i/2998152.jpg',
+		opinion: 'This book is a must-read for every JavaScript developer.',
+		author: 'Douglas Crockford'
 	}
 ]
